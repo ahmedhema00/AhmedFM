3f34559ccd72ae47d5ef83574d0657a4c16d1106
ahmedhema00 <ahmedhema0005@gmail.com>
2026-04-07 19:36:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,16 @@
-self.addEventListener('fetch', () => {});
+const cacheName = 'ahmedfm-v1';
+const assets = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', fetchEvent => {
+  fetchEvent.respondWith(
+    fetch(fetchEvent.request).catch(() => caches.match(fetchEvent.request))
+  );
+});
